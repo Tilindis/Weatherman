d07a977486c0b78a8767516937a3f45c9d430f16
D. Tilindis <heronightmares@gmail.com>
2025-03-25 23:32:34 +0200

    Add init Koin for android and ios

--- composeApp/src/iosMain/kotlin/com/peak/weatherman/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/peak/weatherman/MainViewController.kt
@@ -1,5 +1,6 @@
 package com.peak.weatherman
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.peak.weatherman.utils.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(configure = { initKoin() }) { App() }
