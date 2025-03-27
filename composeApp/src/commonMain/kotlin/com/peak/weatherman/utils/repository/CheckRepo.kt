065b7277854f6974e6fa4a109c8981362e914538
D. Tilindis <heronightmares@gmail.com>
2025-03-27 11:09:50 +0200

    Add db repo to weather repo methods

--- composeApp/src/commonMain/kotlin/com/peak/weatherman/utils/repository/CheckRepo.kt
+++ composeApp/src/commonMain/kotlin/com/peak/weatherman/utils/repository/CheckRepo.kt
@@ -1,5 +1,10 @@
 package com.peak.weatherman.utils.repository
 
+import com.peak.weatherman.utils.domain.Weather
+
 interface CheckRepo {
     fun getText(): String
+    fun getWeathermanData(): List<Weather>
+    fun insertWeathermanData(data: List<Weather>)
+    fun removeAllWeathermanData()
 }
--- composeApp/src/commonMain/kotlin/com/peak/weatherman/utils/repository/CheckRepoImpl.kt
+++ composeApp/src/commonMain/kotlin/com/peak/weatherman/utils/repository/CheckRepoImpl.kt
@@ -1,7 +1,24 @@
 package com.peak.weatherman.utils.repository
 
-class CheckRepoImpl: CheckRepo {
+import com.peak.weatherman.utils.database.LocalDatabase
+import com.peak.weatherman.utils.domain.Weather
+
+class CheckRepoImpl(
+    private val localDatabase: LocalDatabase
+): CheckRepo {
     override fun getText(): String {
         return "Possible to get Weather"
+    }
+
+    override fun getWeathermanData(): List<Weather> {
+        return localDatabase.getWeathermanData()
+    }
+
+    override fun insertWeathermanData(data: List<Weather>) {
+        localDatabase.insertWeathermanData(data)
+    }
+
+    override fun removeAllWeathermanData() {
+        TODO("Not yet implemented")
     }
 }
