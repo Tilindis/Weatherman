6b256eb6c0d63046044aa356183ae20d08fce748
D. Tilindis <heronightmares@gmail.com>
2025-04-07 19:17:24 +0300

    Add injections for main items

--- composeApp/src/androidMain/kotlin/com/peak/weatherman/utils/di/Modules.android.kt
+++ composeApp/src/androidMain/kotlin/com/peak/weatherman/utils/di/Modules.android.kt
@@ -1,6 +1,8 @@
 package com.peak.weatherman.utils.di
 
 import com.peak.weatherman.utils.database.AndroidDatabaseDriverFactory
+import com.peak.weatherman.utils.dispatcher.AndroidDispatcherProvider
+import com.peak.weatherman.utils.dispatcher.DispatcherProvider
 import com.peak.weatherman.utils.factory.DatabaseDriverFactory
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -8,4 +10,5 @@ import org.koin.dsl.module
 
 actual val platformModule = module {
     singleOf(::AndroidDatabaseDriverFactory).bind<DatabaseDriverFactory>()
+    singleOf(::AndroidDispatcherProvider).bind<DispatcherProvider>()
 }
